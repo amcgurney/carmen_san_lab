b3e7494b5cecb5435cebcbd5eb931716c020e836
Alan McGurney <mcgurneyalan@gmail.com>
2022-02-04 16:02:24 -0500

    finished deliverable

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -60,7 +60,7 @@ SELECT * FROM city WHERE id = 211;
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+SELECT * FROM city WHERE population = 91084;
 
 
-
--- She's in ______________________!
+-- She's in _Santa Monica!!!_____________________!
