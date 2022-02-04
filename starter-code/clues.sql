5b15018e4e5d33665835680a3298a348fe07a0c1
Alan McGurney <mcgurneyalan@gmail.com>
2022-02-04 15:34:12 -0500

    clue 5 finished

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -24,18 +24,20 @@ SELECT * FROM country c LEFT JOIN countrylanguage cl ON c.code = cl.countrycode 
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
 SELECT * FROM city WHERE countrycode = 'SMR' AND name != 'San Marino';
---Serravalle
+--serravalle
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
-
+SELECT * FROM city WHERE name LIKE 'Serra%';
+--serra
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
+ 
 
 
 
